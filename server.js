87b5d600aa69f6a6740c0920f9f8e79915d56729
Kevin Parmar <parmarkevinb@gmail.com>
2020-03-12 13:05:37 -0400

    fixed link to jawsdb on server.js

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultlayout: "main"}));
 app.set("view engine", "handlebars");
 
+var connection;
 
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else {
-    const connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost", 
         port: 3306, 
         user: "root", 
